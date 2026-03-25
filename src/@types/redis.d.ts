94d12f27b238a132140921fdda367f4935c70238
anmol420 <anmolanand2k15@gmail.com>
2026-03-26 01:41:11 +0530

    Added type for url param

--- src/@types/redis.d.ts
+++ src/@types/redis.d.ts
@@ -1,6 +1,18 @@
 import { RedisOptions } from "ioredis";
 
+type redisURLOptions = Omit<
+  RedisOptions,
+  "host" | "port" | "username" | "password"
+>;
+
+export type RedisConnectURLConfig = {
+  type: "RedisConnectURLConfig";
+  url: string;
+  options?: redisURLOptions;
+};
+
 export type RedisConnectConfig = {
+  type: "RedisConnectConfig";
   port: number;
   username: string;
   password: string;
