02a0f731df26233db0c3ee369c04e994dc438151
anmol420 <anmolanand2k15@gmail.com>
2026-03-28 01:01:30 +0530

    added changes to exporting of types and removal of type field from rest of types

--- src/@types/redis.d.ts
+++ src/@types/redis.d.ts
@@ -1,18 +1,16 @@
 import { RedisOptions } from "ioredis";
 
-type redisURLOptions = Omit<
+export type RedisURLOptions = Omit<
   RedisOptions,
   "host" | "port" | "username" | "password"
 >;
 
 export type RedisConnectURLConfig = {
-  type: "RedisConnectURLConfig";
   url: string;
   options?: redisURLOptions;
 };
 
 export type RedisConnectConfig = {
-  type: "RedisConnectConfig";
   port: number;
   username: string;
   password: string;
