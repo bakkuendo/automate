960adcecb1d49c7c70769447d53451cf6354efb2
anmol420 <anmolanand2k15@gmail.com>
2026-03-28 01:02:00 +0530

    changed the connect functionality

--- src/libs/redisClient.ts
+++ src/libs/redisClient.ts
@@ -1,11 +1,59 @@
-import Redis, { Redis as RedisClientType } from "ioredis";
-import { RedisConnectConfig, RedisConnectURLConfig } from "../@types/redis";
+import Redis, { Redis as RedisClientType, RedisOptions } from "ioredis";
+import {
+  RedisConnectConfig,
+  RedisConnectURLConfig,
+  RedisURLOptions,
+} from "../@types/redis";
 
 export class RedisClient {
   private static instance: RedisClient | null = null;
   private client: RedisClientType | null = null;
 
   private constructor() {}
+
+  private generateURLOptions(
+    opt: RedisURLOptions | undefined,
+  ): RedisURLOptions {
+    let option: RedisURLOptions = {
+      maxRetriesPerRequest: 3,
+      connectTimeout: 5000,
+      lazyConnect: true,
+    };
+    if (!opt || !opt?.maxRetriesPerRequest || !opt?.connectTimeout) {
+      option = {
+        ...opt,
+        maxRetriesPerRequest: opt?.maxRetriesPerRequest
+          ? opt?.maxRetriesPerRequest
+          : option.maxRetriesPerRequest,
+        connectTimeout: opt?.connectTimeout
+          ? opt?.connectTimeout
+          : option.connectTimeout,
+        lazyConnect: option.lazyConnect,
+      };
+    }
+    return option;
+  }
+
+  private generateConfigOptions(opt: RedisOptions | undefined): RedisOptions {
+    let option: RedisOptions = {
+      maxRetriesPerRequest: 3,
+      connectTimeout: 5000,
+      lazyConnect: true,
+    };
+    if (!opt || !opt?.maxRetriesPerRequest || !opt?.connectTimeout) {
+      option = {
+        ...opt,
+        maxRetriesPerRequest: opt?.maxRetriesPerRequest
+          ? opt?.maxRetriesPerRequest
+          : option.maxRetriesPerRequest,
+        connectTimeout: opt?.connectTimeout
+          ? opt?.connectTimeout
+          : option.connectTimeout,
+        lazyConnect: option.lazyConnect,
+      };
+    }
+    return option;
+  }
 
   public static getInstance(): RedisClient {
     if (!RedisClient.instance) {
@@ -21,90 +69,60 @@ export class RedisClient {
     return this.client;
   }
 
-  public async connect(url: RedisConnectURLConfig): Promise<void>;
+  public async connectURL(url: RedisConnectURLConfig): Promise<void>;
 
-  public async connect(config: RedisConnectConfig): Promise<void>;
-
-  public async connect(
-    arg: RedisConnectURLConfig | RedisConnectConfig,
-  ): Promise<void> {
+  public async connectURL(arg: RedisConnectURLConfig): Promise<void> {
     if (this.client) {
       throw new Error("Already connected to a redis instance.");
-      return;
     }
+    console.log(arg.options);
+    arg.options = this.generateURLOptions(arg.options);
+    console.log(arg.options);
+    let url: string;
     if (
-      !arg.options ||
-      !arg.options?.maxRetriesPerRequest ||
-      !arg.options?.connectTimeout ||
-      !arg.options?.lazyConnect
+      arg.url.split(":")[0] === "redis" ||
+      arg.url.split(":")[0] === "rediss"
     ) {
-      arg.options = {
-        maxRetriesPerRequest: arg.options?.maxRetriesPerRequest
-          ? arg.options?.maxRetriesPerRequest
-          : 3,
-        connectTimeout: arg.options?.connectTimeout
-          ? arg.options?.connectTimeout
-          : 5000,
-        lazyConnect: arg.options?.lazyConnect ? arg.options?.lazyConnect : true,
-        ...arg.options,
-      };
+      url = arg.url;
+    } else {
+      throw new Error(
+        `Invalid Redis URL: "${arg}". Expected format: redis://user:password@host:port`,
+      );
     }
-    if (arg.type === "RedisConnectURLConfig") {
-      let url: string;
-      if (
-        arg.url.split(":")[0] === "redis" ||
-        arg.url.split(":")[0] === "rediss"
-      ) {
-        url = arg.url;
-      } else {
-        throw new Error(
-          `Invalid Redis URL: "${arg}". Expected format: redis://user:password@host:port`,
-        );
-      }
-      new Promise((resolve, reject) => {
-        const client: Redis = new Redis(url, {
-          ...arg.options,
-        });
-        client.on("error", (err) => {
-          throw new Error("Redis error: ", err);
-        });
-        client
-          .connect()
-          .then(() => {
-            this.client = client;
-            resolve(this.client);
-          })
-          .catch((err) => {
-            reject(new Error(`Failed to connect to Redis: ${err.message}`));
-          });
-      });
-      return;
+    const client: Redis = new Redis(url, {
+      ...arg.options,
+    });
+    client.on("error", (err) => {
+      throw new Error("Redis error: ", err);
+    });
+    await client.connect();
+    this.client = client;
+    return;
+  }
+
+  public async connectConfig(config: RedisConnectConfig): Promise<void>;
+
+  public async connectConfig(arg: RedisConnectConfig): Promise<void> {
+    if (this.client) {
+      throw new Error("Already connected to a redis instance.");
     }
+    arg.options = this.generateConfigOptions(arg.options);
     if (!arg.username || !arg.port || !arg.password) {
       throw new Error(
         "Connection `username`,`port` or `password` not provided.",
       );
     }
-    new Promise((resolve, reject) => {
-      const client: Redis = new Redis({
-        port: arg.port,
-        username: arg.username,
-        password: arg.password,
-        ...arg.options,
-      });
-      client.on("error", (err) => {
-        throw new Error("Redis error: ", err);
-      });
-      client
-        .connect()
-        .then(() => {
-          this.client = client;
-          resolve(this.client);
-        })
-        .catch((err) => {
-          reject(new Error(`Failed to connect to Redis: ${err.message}`));
-        });
+    const client: Redis = new Redis({
+      port: arg.port,
+      username: arg.username,
+      password: arg.password,
+      ...arg.options,
     });
+    client.on("error", (err) => {
+      throw new Error("Redis error: ", err);
+    });
+    await client.connect();
+    this.client = client;
     return;
   }
 
